707cb009ee13aeb1a33dd5ce5bccf92a587a4a49
NikosBakalis <bakalis.nikolas@gmail.com>
2020-03-26 20:19:57 +0200

    print_csv ready

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("This is the main file!")
+import csv
+
+
+def print_csv(csv_path):
+    """
+    This function takes as argument the path of a csv file and then it prints the csv file
+    """
+    with open(csv_path) as csv_path:
+        reader = csv.reader(csv_path)
+        for row in reader:
+            print("\t | ".join(row))
+
+
+print_csv("C:\\Users\\Nikolas\\PycharmProjects\\Support_Vector_Machines\\Input\\winequality-red.csv")
+
