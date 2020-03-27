a317ef2b5d5a5ab089bc76432a59238ef42fcffe
NikosBakalis <bakalis.nikolas@gmail.com>
2020-03-27 20:35:25 +0200

    Example of f1 score

--- src/main.py
+++ src/main.py
@@ -4,10 +4,9 @@ from Linker import linker
 
 # print_csv("C:\\Users\\Nikolas\\PycharmProjects\\Support_Vector_Machines\\Input\\winequality-red.csv")
 # new_list = csv_to_1d_list("C:\\Users\\Nikolas\\PycharmProjects\\Support_Vector_Machines\\Input\\winequality-red.csv")
-e = linker.csv_column_to_list("C:\\Users\\Nikolas\\PycharmProjects\\Support_Vector_Machines\\Input\\winequality-red.csv", 2)
-print(e)
+print(linker.csv_column_to_list("C:\\Users\\Nikolas\\PycharmProjects\\Support_Vector_Machines\\Input\\winequality-red.csv", 2))
 
 y_true = [0, 1, 2, 0, 1, 2]
-y_pred = [0, 1, 2, 0, 1, 2]
-print(f1_score(e, e, average='macro'))
+y_pred = [0, 0, 1, 1, 3, 2]
+print(f1_score(y_true, y_pred, average='macro'))
 
