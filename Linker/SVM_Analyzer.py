16f5eb4fd0e4cdfab25e5211101068f1410cea85
NikosBakalis <bakalis.nikolas@gmail.com>
2020-03-29 21:40:52 +0300

    1st attempt. To fix!

---
+++ Linker/SVM_Analyzer.py
@@ -0,0 +1,95 @@
+# Libraries for analysis
+# import sys
+import pandas
+import numpy
+from sklearn import svm
+from Linker import linker
+from sklearn.metrics import f1_score, precision_recall_fscore_support
+
+# Libraries for visuals
+import matplotlib.pyplot as plt
+import seaborn
+
+seaborn.set(font_scale=1.2)
+
+
+# Allow charts to appear in the notebook
+# %matplotlib inline
+
+data = pandas.read_csv("C:\\Users\\Nikolas\\PycharmProjects\\Wine_Quality\\Input\\winequality-red-test.csv")
+if data.empty:
+    all_data = linker.csv_to_list("C:\\Users\\Nikolas\\PycharmProjects\\Wine_Quality\\Input\\winequality-red.csv")
+    print(all_data)
+    # all_data.pop(0)
+    # print(all_data)
+    # all_data = [float(j) for j in all_data]
+    # print(all_data)
+    all_data_length = all_data.__len__()
+    all_data_starting_length_to_test = all_data_length * 75 / 100
+    print(int(all_data_starting_length_to_test))
+    all_data_to_test = []
+    for i in range(0, int(all_data_starting_length_to_test)):
+        all_data_to_test.append(all_data[i])
+    print(all_data_to_test)
+    print(all_data_to_test.__len__())
+
+    pandas.DataFrame(all_data_to_test).to_csv(
+        "C:\\Users\\Nikolas\\PycharmProjects\\Wine_Quality\\Input\\winequality-red-test.csv", header=None, index=None)
+
+    data = pandas.read_csv("C:\\Users\\Nikolas\\PycharmProjects\\Wine_Quality\\Input\\winequality-red-test.csv")
+    data.head()
+else:
+    print("FFFFFFFFFFFFFFFFFFF")
+    data.head()
+
+seaborn.lmplot("fixed acidity", "volatile acidity", data=data, hue="quality", palette="Set1", fit_reg=False,
+               scatter_kws={"s": 70})
+plt.show()
+
+# Specify inputs for the model.
+# acidities = data[["fixed acidity", "volatile acidity"]].to_numpy()
+acidities = data[
+    ["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides", "free sulfur dioxide",
+     "total sulfur dioxide", "density", "pH", "sulphates", "alcohol"]].to_numpy()
+type_label = numpy.where(data["quality"] == 1, data["quality"] - 1, data["quality"])
+# numpy.set_printoptions(threshold=sys.maxsize)
+print(type_label)
+
+# Fit the SVM model.
+model = svm.SVC(kernel="linear", decision_function_shape="ovr")
+model.fit(acidities, type_label)
+
+# Get the separating hyperplane
+w = model.coef_[0]
+a = -w[0] / w[1]
+xx = numpy.linspace(-100, 100)
+yy = a * xx - (model.intercept_[0]) / w[1]
+
+# Plot the parallels to the separating hyperplane that pass through the support vectors
+b = model.support_vectors_[0]
+yy_down = a * xx + (b[1] - a * b[0])
+b = model.support_vectors_[-1]
+yy_up = a * xx + (b[1] - a * b[0])
+
+# Look the margins and support vectors
+seaborn.lmplot("fixed acidity", "volatile acidity", data=data, hue="quality", palette="Set1", fit_reg=False,
+               scatter_kws={"s": 70})
+plt.plot(xx, yy, linewidth=2, color="black")
+plt.plot(xx, yy_down, "k--")
+plt.plot(xx, yy_up, "k--")
+plt.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1], s=80, facecolors="none")
+plt.show()
+
+# Plot the hyperplane
+seaborn.lmplot("fixed acidity", "volatile acidity", data=data, hue="quality", palette="Set1", fit_reg=False,
+               scatter_kws={"s": 70})
+plt.plot(xx, yy, linewidth=2, color="black")
+# plt.plot(-2, -100, "yo", markersize="9")
+plt.plot(-100, -100, 0.0, 1.9, 0.076, 11.0, 34.0, 0.9978, 3.51, 0.56, 9.4, "yo", markersize="9")
+plt.show()
+
+
+def determine(fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide,
+              total_sulfur_dioxide, density, pH, sulphates, alcohol):
+    print(model.predict([[fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides,
+                          free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]]))
--- src/main.py
+++ src/main.py
@@ -1,108 +1,59 @@
-# Libraries for analysis
-import pandas
-import numpy
-from sklearn import svm
-from sklearn.metrics import f1_score, precision_recall_fscore_support
-
 # Basic library for all my methods
+import pandas
+
 from Linker import linker
+from Linker import SVM_Analyzer
 
-# Libraries for visuals
-import matplotlib.pyplot as plt
-import seaborn
-seaborn.set(font_scale=1.2)
-
-# Allow charts to appear in the notebook
-# %matplotlib inline
+import csv
+import numpy
 
 
-data = pandas.read_csv("C:\\Users\\Nikolas\\PycharmProjects\\Support_Vector_Machines\\Input\\winequality-red-test.csv")
-# data = pandas.read_excel("C:\\Users\\Nikolas\\PycharmProjects\\Support_Vector_Machines\\Input\\test.xlsx")
-data.head()
+data = linker.csv_column_to_list("C:\\Users\\Nikolas\\PycharmProjects\\Wine_Quality\\Input\\winequality-red.csv", 11)
+data.pop(0)
+data = [int(i) for i in data]
+print(data)
 
-seaborn.lmplot("fixed acidity", "volatile acidity", data=data, hue="quality", palette="Set1", fit_reg=False, scatter_kws={"s": 70})
-plt.show()
+print("RRRRRRRRRRR")
 
-# Specify inputs for the model.
-acidities = data[["fixed acidity", "volatile acidity"]].to_numpy()
-# acidities = data[["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density", "pH", "sulphates", "alcohol"]].to_numpy()
-type_label1 = numpy.where(data["quality"] == 5, 0, 1)
-type_label2 = numpy.where(data["quality"] == 7, 1, 2)
-
-# Fit the SVM model.
-model = svm.SVC(kernel="linear", decision_function_shape="ovr")
-model.fit(acidities, type_label1)
-model.fit(acidities, type_label2)
-
-# Get the separating hyperplane
-w = model.coef_[0]
-a = -w[0] / w[1]
-xx = numpy.linspace(-100, 100)
-yy = a * xx - (model.intercept_[0]) / w[1]
-
-# Plot the parallels to the separating hyperplane that pass through the support vectors
-b = model.support_vectors_[0]
-yy_down = a * xx + (b[1] - a * b[0])
-b = model.support_vectors_[-1]
-yy_up = a * xx + (b[1] - a * b[0])
-
-# Look the margins and support vectors
-seaborn.lmplot("fixed acidity", "volatile acidity", data=data, hue="quality", palette="Set1", fit_reg=False, scatter_kws={"s": 70})
-plt.plot(xx, yy, linewidth=2, color="black")
-plt.plot(xx, yy_down, "k--")
-plt.plot(xx, yy_up, "k--")
-plt.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1], s=80, facecolors="none")
-plt.show()
-
-# Plot the hyperplane
-seaborn.lmplot("fixed acidity", "volatile acidity", data=data, hue="quality", palette="Set1", fit_reg=False, scatter_kws={"s": 70})
-plt.plot(xx, yy, linewidth=2, color="black")
-plt.plot(-2, -100, "yo", markersize="9")
-# plt.plot(-100, -100, 0.0, 1.9, 0.076, 11.0, 34.0, 0.9978, 3.51, 0.56, 9.4, "yo", markersize="9")
-plt.show()
+# print(data.__len__())
+data_length = data.__len__()
+data_starting_length_to_test = data_length * 75/100
+print(int(data_starting_length_to_test))
+data_to_test = []
+for i in range(int(data_starting_length_to_test), data.__len__()):
+    data_to_test.append(data[i])
+print(data_to_test)
+print(data_to_test.__len__())
 
 
-def determine(fixed_acidity, volatile_acidity):
-    if model.predict([[fixed_acidity, volatile_acidity]]) == 0:
-        print(5)
-    elif model.predict([[fixed_acidity, volatile_acidity]]) == 1:
-        print(7)
-    elif model.predict([[fixed_acidity, volatile_acidity]]) == 2:
-        print(10)
+print("DDD")
 
 
-# def determine(fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol):
-#     if model.predict([[fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]]) == 0:
-#         print(5)
-#     else:
-#         print(7)
+svm_data = SVM_Analyzer.type_label.tolist()
+print(svm_data)
 
 
-print(model.predict([[-2, -100]]))
-print(model.predict([[244, -100]]))
-print(model.predict([[4234, -500]]))
-print(model.predict([[24.4324, 400]]))
-print(model.predict([[44.44, 0]]))
-print(model.predict([[12323, -134]]))
-print(model.predict([[-100, -4]]))
-
-determine(1, 1)
-determine(7.4, 0.7)
-determine(7.3, 0.65)
-determine(-100, -4)
+if data == svm_data:
+    print("equal")
+else:
+    print("not equal")
 
 
-# print(model.predict([[-2, -100, 0.40, 1.2, 0.065, -5.0, -21.0, 4.2143, 33.39, -0.47, 510.0]]))
-# print(model.predict([[244, -100, 0.0324, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0]]))
-# print(model.predict([[4234, -500, 0.044, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0]]))
-# print(model.predict([[24.4324, 400, 0.023423, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0]]))
-# print(model.predict([[44.44, 0, 0.024, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0]]))
-# print(model.predict([[12323, -134, 0.04344, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0]]))
-# print(model.predict([[-100, -4, 224.0, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0]]))
-#
-# determine(7.4, 0.7, 0.0, 1.9, 0.076, 11.0, 34.0, 0.9978, 3.51, 0.56, 9.4)
-# determine(7.3, 0.65, 0.0, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0)
-# determine(-2, -100, 0.40, 1.2, 0.065, -5.0, -21.0, 4.2143, 33.39, -0.47, 510.0)
+# SVM_Analyzer.determine(7.4, 0.59, 0.08, 4.4, 0.086, 6.0, 29.0, 0.9974, 3.38, 0.5, 9.0)
+# SVM_Analyzer.determine(8.1, 0.56, 0.28, 1.7, 0.368, 16.0, 56.0, 0.9968, 3.11, 1.28, 9.3)
+# SVM_Analyzer.determine(-2, -100, 0.40, 1.2, 0.065, -5.0, -21.0, 4.2143, 33.39, -0.47, 510.0)
+# SVM_Analyzer.determine(244, -100, 0.0324, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0)
+# SVM_Analyzer.determine(4234, -500, 0.044, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0)
+# SVM_Analyzer.determine(24.4324, 400, 0.023423, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0)
+# SVM_Analyzer.determine(44.44, 0, 0.024, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0)
+# SVM_Analyzer.determine(12323, -134, 0.04344, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0)
+# SVM_Analyzer.determine(-100, -4, 224.0, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0)
+# SVM_Analyzer.determine(7.4, 0.7, 0.0, 1.9, 0.076, 11.0, 34.0, 0.9978, 3.51, 0.56, 9.4)
+# SVM_Analyzer.determine(7.3, 0.65, 0.0, 1.2, 0.065, 15.0, 21.0, 0.9946, 3.39, 0.47, 10.0)
+# SVM_Analyzer.determine(-2, -100, 0.40, 1.2, 0.065, -5.0, -21.0, 4.2143, 33.39, -0.47, 510.0)
+# SVM_Analyzer.determine(2222, 2222, 2222, 2222, 2222, 2222, 2222, 2222, 2222, 2222, 2222)
+# SVM_Analyzer.determine(-2222, -2222, -2222, -2222, -2222, -2222, -2222, -2222, -2222, -2222, -2222)
+
 
 
 # print(linker.csv_column_to_list("C:\\Users\\Nikolas\\PycharmProjects\\Support_Vector_Machines\\Input\\winequality-red.csv", 11))
