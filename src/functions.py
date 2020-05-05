899c531e6f3ae6ecc214e0c6b021386d5c692a78
NikosBakalis <bakalis.nikolas@gmail.com>
2020-05-05 16:42:29 +0300

    Scores for B added!!! TODO: Add comments.

---
+++ src/functions.py
@@ -0,0 +1,56 @@
+import sys
+
+import numpy
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
+
+
+def average(X_train, X_test, y_train, y_test):
+    X_train_Average = X_train
+    X_test_Average = X_test
+    y_train_Average = y_train
+    y_test_Average = y_test
+    print("I am learning...")
+    average = sum(y_train_Average) / len(y_train_Average)
+    y_test_new = [round(average, 2) for x in y_test_Average]
+    return y_test_new
+
+
+def logistic_regression(X_train, X_test, y_train, y_test):
+    X_train_Logistic_Regression = X_train
+    X_test_Logistic_Regression = X_test
+    y_train_Logistic_Regression = y_train
+    y_test_Logistic_Regression = y_test
+    y_train_encoded = [x * 100 for x in y_train_Logistic_Regression]
+    model = LogisticRegression(max_iter=10000)
+    print("I am learning...")
+    # X_test.drop(["pH"], axis=1, inplace=True)
+    model.fit(X_train_Logistic_Regression, y_train_encoded)
+    y_test_decoded = [x / 100 for x in model.predict(X_test_Logistic_Regression)]
+    return y_test_decoded
+
+
+def k_means(X_train, X_test, y_train, y_test):
+    X_train_K_means = X_train
+    X_test_K_means = X_test
+    y_train_K_means = y_train
+    y_test_K_means = y_test
+    clusters = input("Now choose number of clusters: ")
+    numpy.set_printoptions(threshold=sys.maxsize, suppress=True)
+    model = KMeans(n_clusters=int(clusters))
+    # X_test.drop(["pH"], axis=1, inplace=True)
+    X_train_K_means.insert(8, "pH", y_train_K_means, True)
+    X_test_K_means.insert(8, "pH", y_test_K_means, True)
+    # print(X_train_K_means)
+    print("I am learning...")
+    model.fit_predict(X_train_K_means, y_train_K_means)
+    for k in (range(int(clusters))):
+        print("pH for cluster no.", int(k), "is:", round(model.cluster_centers_[k][8], 2))
+    what_I_want = model.predict(X_test_K_means)
+    what_I_really_want = []
+    # X_test.insert(8, "pH", y_test_decoded, True)
+    for k in (range(len(what_I_want))):
+        what_I_really_want.append(round(model.cluster_centers_[what_I_want][k][8], 2))
+    X_train_K_means.drop(["pH"], axis=1, inplace=True)
+    X_test_K_means.drop(["pH"], axis=1, inplace=True)
+    return what_I_really_want
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os.path
 import sys
 
 import numpy
@@ -7,34 +8,55 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.cluster import KMeans
+from Linker import linker
+
+linker.csv_clear("../Input/winequality-red-Average.csv")
+linker.csv_clear("../Input/winequality-red-Logistic-Regression.csv")
+linker.csv_clear("../Input/winequality-red-K-means.csv")
 
 question = ""
 while question != "Leave":
     print("What would you like to do?")
     question = input("A, B or Leave: ")
-
     df = pandas.read_csv("../Input/winequality-red.csv")
-    X_train, X_test, y_train, y_test = train_test_split(df[["fixed acidity", "volatile acidity", "citric acid",
-                                                            "residual sugar", "chlorides", "free sulfur dioxide",
-                                                            "total sulfur dioxide", "density", "pH", "sulphates",
-                                                            "alcohol"]], df.quality, test_size=0.25)
-
+    X_train_A, X_test_A, y_train_A, y_test_A = train_test_split(
+        df[["fixed acidity", "volatile acidity", "citric acid",
+            "residual sugar", "chlorides",
+            "free sulfur dioxide", "total sulfur dioxide", "density", "pH",
+            "sulphates", "alcohol"]], df.quality, test_size=0.25)
     if question == "A":
         print("This is A")
-        clf = SVC(gamma='auto')
-        clf.fit(X_train, y_train)
-        final = clf.predict(X_test)
-        # print(y_test, final)
-
-        print("f1 Score:\t\t\t", float(f1_score(y_test, final, average='micro')))
-        print("Precision Score:\t", float(precision_score(y_test, final, average='micro')))
-        print("Recall Score:\t\t", float(recall_score(y_test, final, average='micro')), "\n\n")
-
+        file = ""
+        print("Now choose the file to SVM.")
+        print("Possible files.")
+        file = input("winequality-red, winequality-red-Average, "
+                     "winequality-red-Logistic-Regression and winequality-red-K-means:")
+        if os.path.exists("../Input/" + file + ".csv"):
+            if os.stat("../Input/" + file + ".csv").st_size != 0:
+                df = pandas.read_csv("../Input/" + file + ".csv")
+                X_train_A, X_test_A, y_train_A, y_test_A = train_test_split(
+                    df[["fixed acidity", "volatile acidity", "citric acid",
+                        "residual sugar", "chlorides",
+                        "free sulfur dioxide", "total sulfur dioxide", "density", "pH",
+                        "sulphates", "alcohol"]], df.quality, test_size=0.25)
+                clf = SVC(gamma='auto')
+                clf.fit(X_train_A, y_train_A)
+                final = clf.predict(X_test_A)
+                # print(y_test, final)
+                print("f1 Score:\t\t\t", float(f1_score(y_test_A, final, average='micro')))
+                print("Precision Score:\t", float(precision_score(y_test_A, final, average='micro')))
+                print("Recall Score:\t\t", float(recall_score(y_test_A, final, average='micro')), "\n\n")
+            else:
+                print("This file is empty.")
+                print("Try again with another file or populate this file using B.")
+        else:
+            print("There is no file with that name.")
+            print("Please try again with one of the given names.")
     elif question == "B":
         print("This is B")
         choice = ""
-        X_train['quality'] = y_train
-        df2 = X_train
+        X_train_A['quality'] = y_train_A
+        df2 = X_train_A
         X_train, X_test, y_train, y_test = train_test_split(df2[["fixed acidity", "volatile acidity", "citric acid",
                                                                  "residual sugar", "chlorides",
                                                                  "free sulfur dioxide",
@@ -43,72 +65,57 @@ while question != "Leave":
         while choice != "Leave":
             choice = input("Choose one between Average, Logistic Regression, K-means or Leave: ")
             # print(X_train, X_test, y_train, y_test)
+            import functions
             if choice == "Average":
                 print("Average")
-                X_train_Average = X_train
-                X_test_Average = X_test
-                y_train_Average = y_train
-                y_test_Average = y_test
-                average = sum(y_train_Average) / len(y_train_Average)
-                y_test_new = [round(average, 2) for x in y_test_Average]
-                # X_test_Average.insert(8, "pH", y_test_new, True)
-                print(y_test_new)
-                y_test_new = [round(x * 100) for x in y_test_new]
-                y_test_Average = [round(x * 100) for x in y_test]
-                print("f1 Score:\t\t\t", round(float(f1_score(y_test_Average, y_test_new, average='micro')), 2))
-                print("Precision Score:\t",
-                      round(float(precision_score(y_test_Average, y_test_new, average='micro')), 2))
-                print("Recall Score:\t\t", round(float(recall_score(y_test_Average, y_test_new, average='micro')), 2),
-                      "\n\n")
+                result = functions.average(X_train, X_test, y_train, y_test)
+                X_train.insert(8, "pH", y_train, True)
+                X_test.insert(8, "pH", result, True)
+                X_test_B = X_test_A
+                y_test_B = y_test_A
+                print(X_test_B, y_test_B)
+                X_test_B.insert(11, "quality", y_test_B, True)
+                # print(X_train, X_test)
+                results = X_train.append(X_test, sort=False)
+                results = results.append(X_test_B, sort=False)
+                print(results)
+                results.to_csv("../Input/winequality-red-Average.csv", index=False)
+                X_train.drop(["pH"], axis=1, inplace=True)
+                X_test.drop(["pH"], axis=1, inplace=True)
+                X_test_B.drop(["quality"], axis=1, inplace=True)
             elif choice == "Logistic Regression":
                 print("Logistic Regression")
-                X_train_Logistic_Regression = X_train
-                X_test_Logistic_Regression = X_test
-                y_train_Logistic_Regression = y_train
-                y_test_Logistic_Regression = y_test
-                y_train_encoded = [x * 100 for x in y_train_Logistic_Regression]
-                model = LogisticRegression(max_iter=10000)
-                print("I am learning...")
-                # X_test.drop(["pH"], axis=1, inplace=True)
-                model.fit(X_train_Logistic_Regression, y_train_encoded)
-                y_test_decoded = [x / 100 for x in model.predict(X_test_Logistic_Regression)]
-                # X_test_Logistic_Regression.insert(8, "pH", y_test_decoded, True)
-                print(y_test_decoded)
-                y_test_re_encoded = [round(x * 100) for x in y_test_decoded]
-                y_test_Logistic_Regression = [round(x * 100) for x in y_test]
-                print("f1 Score:\t\t\t",
-                      round(float(f1_score(y_test_Logistic_Regression, y_test_re_encoded, average='micro')), 2))
-                print("Precision Score:\t",
-                      round(float(precision_score(y_test_Logistic_Regression, y_test_re_encoded, average='micro')), 2))
-                print("Recall Score:\t\t",
-                      round(float(recall_score(y_test_Logistic_Regression, y_test_re_encoded, average='micro')), 2),
-                      "\n\n")
+                result = functions.logistic_regression(X_train, X_test, y_train, y_test)
+                X_train.insert(8, "pH", y_train, True)
+                X_test.insert(8, "pH", result, True)
+                # print(X_train, X_test)
+                X_test_B = X_test_A
+                y_test_B = y_test_A
+                print(X_test_B, y_test_B)
+                X_test_B.insert(11, "quality", y_test_B, True)
+                # print(X_train, X_test)
+                results = X_train.append(X_test, sort=False)
+                results = results.append(X_test_B, sort=False)
+                print(results)
+                results.to_csv("../Input/winequality-red-Logistic-Regression.csv", index=False)
+                X_train.drop(["pH"], axis=1, inplace=True)
+                X_test.drop(["pH"], axis=1, inplace=True)
+                X_test_B.drop(["quality"], axis=1, inplace=True)
             elif choice == "K-means":
                 print("K-means")
-                X_train_K_means = X_train
-                X_test_K_means = X_test
-                y_train_K_means = y_train
-                y_test_K_means = y_test
-                clusters = input("Now choose number of clusters: ")
-                numpy.set_printoptions(threshold=sys.maxsize, suppress=True)
-                model = KMeans(n_clusters=int(clusters))
-                # X_test.drop(["pH"], axis=1, inplace=True)
-                X_train_K_means.insert(8, "pH", y_train_K_means, True)
-                X_test_K_means.insert(8, "pH", y_test_K_means, True)
-                given = model.fit_predict(X_train_K_means, y_train_K_means)
-                for k in (range(int(clusters))):
-                    print("pH for cluster no.", int(k), "is:", round(model.cluster_centers_[k][8], 2))
-                what_I_want = model.predict(X_test_K_means)
-                what_I_really_want = []
-                # X_test.insert(8, "pH", y_test_decoded, True)
-                for k in (range(len(what_I_want))):
-                    what_I_really_want.append(round(model.cluster_centers_[what_I_want][k][8], 2))
-                print(what_I_really_want)
-                what_I_really_really_want = [round(x * 100) for x in what_I_really_want]
-                y_test_K_means = [round(x * 100) for x in y_test]
-                print("f1 Score:\t\t\t",
-                      round(float(f1_score(y_test_K_means, what_I_really_really_want, average='micro')), 2))
-                print("Precision Score:\t",
-                      round(float(precision_score(y_test_K_means, what_I_really_really_want, average='micro')), 2))
-                print("Recall Score:\t\t",
-                      round(float(recall_score(y_test_K_means, what_I_really_really_want, average='micro')), 2), "\n\n")
+                result = functions.k_means(X_train, X_test, y_train, y_test)
+                X_train.insert(8, "pH", y_train, True)
+                X_test.insert(8, "pH", result, True)
+                # print(X_train, X_test)
+                X_test_B = X_test_A
+                y_test_B = y_test_A
+                print(X_test_B, y_test_B)
+                X_test_B.insert(11, "quality", y_test_B, True)
+                # print(X_train, X_test)
+                results = X_train.append(X_test, sort=False)
+                results = results.append(X_test_B, sort=False)
+                print(results)
+                results.to_csv("../Input/winequality-red-K-means.csv", index=False)
+                X_train.drop(["pH"], axis=1, inplace=True)
+                X_test.drop(["pH"], axis=1, inplace=True)
+                X_test_B.drop(["quality"], axis=1, inplace=True)
